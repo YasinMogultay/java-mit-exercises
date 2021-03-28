63c92da09be84bd2a88ffe8616704cf9c33b5bf9
Yasin Mogultay <yasin.mglty@gmail.com>
2021-03-28 17:58:57 -0500

    first question is completed

---
+++ src/EdabitJavaExercises.java
@@ -0,0 +1,11 @@
+public class EdabitJavaExercises {
+
+    public static void main(String[] args) {
+        System.out.println(convert(5));
+    }
+
+    //Write a function that takes an integer minutes and converts it to seconds.
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
+}
