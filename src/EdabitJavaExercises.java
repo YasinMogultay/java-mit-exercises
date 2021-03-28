e0b2122d4c2232a30c4b52d5652ba9a4a76428a2
Yasin Mogultay <yasin.mglty@gmail.com>
2021-03-28 18:09:12 -0500

    completed 2. challenge

--- src/EdabitJavaExercises.java
+++ src/EdabitJavaExercises.java
@@ -2,10 +2,16 @@ public class EdabitJavaExercises {
 
     public static void main(String[] args) {
         System.out.println(convert(5));
+        System.out.println(perimeterOfRectangle(5,6));
     }
 
     //Write a function that takes an integer minutes and converts it to seconds.
     public static int convert(int minutes) {
         return minutes * 60;
+    }
+
+    //Create a function that takes length and width and finds the perimeter of a rectangle.
+    public static int perimeterOfRectangle(int length, int width) {
+        return 2 * (length + width);
     }
 }
